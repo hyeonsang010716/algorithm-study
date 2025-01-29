225325954b4e7333362ef6786bfe607708d98fc1
sentryc0201 <yc020102@gmail.com>
2025-01-30 06:11:41 +0900

    Update backend

--- backend/sentryc/view/answer_view.py
+++ backend/sentryc/view/answer_view.py
@@ -1,19 +1,21 @@
 from flask import Blueprint, request, jsonify
-from model.geometry import p1027
+from model.greedy import p1041
 
 ans_bp = Blueprint('answer', __name__)
-@ans_bp.route('/chat/algorithm/', methods=['POST'])
+@ans_bp.route('/chat/txt-algorithm/', methods=['POST'])
 def send_ans():
-    input_list = request.json.get("input")
+    input_file = request.files.get('file')
+    input_list = input_file.read().decode('utf-8').split("\r\n\r\n\r\n")
     
     answers = []
     
     for input in input_list:
-        answer = p1027.solve(input)
+        if input == "": continue
+        answer = p1041.solve(input)
         answers.append(str(answer))
         
     return jsonify({"answer": answers})
 
 @ans_bp.route('/chat/problem_number/', methods=['POST'])
 def send_num():
-    return jsonify({"answer": 1027})
+    return jsonify({"answer": 1041})
