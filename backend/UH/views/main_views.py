12827571b793fc737c1a068e60506794b4884025
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-01-30 14:49:18 +0900

    FEAT: POST 파일 저장 및 제거

--- backend/UH/views/main_views.py
+++ backend/UH/views/main_views.py
@@ -1,14 +1,27 @@
 from flask import Blueprint, request
 from models.main import get_answer
+import os
+
 
 bp = Blueprint('main', __name__, url_prefix='/UH')
 
+upload_folder = "./upload"
 pro_num = 1918
 
-@bp.route("/chat/algorithm", methods=['POST'])
+@bp.route("/chat/txt-algorithm", methods=['POST'])
 def send_result():
-    input_data = request.get_json()["input"]
-    return get_answer(pro_num, input_data)
+    file = request.files['file']
+    
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+    
+    file_path = os.path.join("./upload", "tmp.txt")
+    file.save(file_path)
+
+    print(file_path)
+
+    os.remove(file_path)
+    return {'answer': '1'}, 200
 
 @bp.route("/chat/problem_number", methods=["POST"])
 def send_problem_number():
