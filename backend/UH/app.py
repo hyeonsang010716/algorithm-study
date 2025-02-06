66f3744a5b609c7b96cc1c5afff44b1855abf10a
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-02-06 16:19:04 +0900

    FEAT: 서버 backend 설정

--- backend/UH/app.py
+++ backend/UH/app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+import os
 
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///' + os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")) + '/problems.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    print('INFO: ', app.config['SQLALCHEMY_DATABASE_URI'])
+
+    db.init_app(app)
 
     from views import main_views
     app.register_blueprint(main_views.bp)
